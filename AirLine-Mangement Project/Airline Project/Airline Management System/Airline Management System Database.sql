976900ee906a05af6ebe8afc94d2e19d5e786471
ujwala-paswan-IT2003 <ujwalapaswan09@gmail.com>
2025-01-07 15:49:29 +0530

    Initial commit: Airline Management System project

---
+++ AirLine-Mangement Project/Airline Project/Airline Management System/Airline Management System Database.sql
@@ -0,0 +1,42 @@
+show databases;
+create database airlinemanagementsystem;
+use airlinemanagementsystem;
+
+/*Login Table*/
+
+create table login(username varchar(20), password varchar(20));
+
+insert into login values('admin', 'admin');
+
+show tables;
+select * from login;
+
+/*Passenger Table*/
+
+create table passenger (name varchar(20), nationality varchar(20), phone varchar(15), address varchar(50), aadhar varchar(20), gender varchar(20));
+
+select * from passenger;
+
+/*flight Table*/
+create table flight(f_code varchar(20), f_name varchar(20), source varchar(40), destination varchar(40));
+
+
+
+/*reservation Table*/
+
+create table reservation(PNR varchar(15), TICKET varchar(20), aadhar varchar(20), name varchar(20), nationality varchar(30), flightname varchar(15), flightcode varchar(20), src varchar(30), des varchar(30), ddate varchar(30));
+
+/*cancel Table*/
+
+create table cancel(pnr varchar(20), name varchar(40), cancelno varchar(20), fcode varchar(20), ddate varchar(30));
+
+show tables;
+
+create table flight(f_code varchar(20), f_name varchar(20), source varchar(40), destination varchar(40));
+
+insert into flight values("1001", "AI-1212", "Delhi", "Mumbai");
+insert into flight values("1002", "AI-1453", "Delhi", "Goa");
+insert into flight values("1003", "AI-1112", "Mumbai", "Chennai");
+insert into flight values("1004", "AI-3222", "Delhi", "Amritsar");
+
+show tables;
